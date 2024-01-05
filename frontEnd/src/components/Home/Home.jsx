8ec37cadc1a0a9249ec36df708a5d014da658004
fsananna <sultanafarhana55@gmail.com>
2024-01-05 11:45:37 +0600

    Gap fixed

--- frontEnd/src/components/Home/Home.jsx
+++ frontEnd/src/components/Home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
   return (
     <div className='flex flex-row'>
-      <div className="basis-1/4">
+      <div className="basis-1/5">
         <ul className="text-white font-semibold text-xl bg-black menu menu-sm h-screen z-[1] p-2 shadow w-52 hidden md:block">
           <li><a className="hover:text-orange-500">Dashboard</a></li>
           <li><a className="hover:text-orange-500">Contact</a></li>
